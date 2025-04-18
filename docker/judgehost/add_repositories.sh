4e8f2c34824de079f1ad7379cd401666b91bf65d
Joshua Gort <accounts@joshuagort.com>
2025-04-18 18:27:55 +0200

    Changed kotlin to chipcie repo, upgrade to 2.1.20

--- docker/judgehost/add_repositories.sh
+++ docker/judgehost/add_repositories.sh
@@ -1,9 +1,9 @@
 apt-get update
 apt-get install software-properties-common gnupg
 
-echo "deb [trusted=yes] https://sysopspackages.icpc.global/apt/jammy jammy main" >> /etc/apt/sources.list
-echo 'Acquire::https::sysopspackages.icpc.global::Verify-Peer "false";
-Acquire::https::sysopspackages.icpc.global::Verify-Host "false";' >> /etc/apt/apt.conf.d/80trust-baylor-mirror
+echo "deb [trusted=yes] https://wisvch.github.io/chipcie-repo/debian ." >> /etc/apt/sources.list
+echo 'Acquire::wisvch.github.io::Verify-Peer "false";
+Acquire::https::wisvch.github.io::Verify-Host "false";' >> /etc/apt/apt.conf.d/80trust-baylor-mirror
 
 # Add pypy repo
 add-apt-repository ppa:pypy/ppa
