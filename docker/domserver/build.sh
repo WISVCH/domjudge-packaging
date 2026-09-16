480777862533042dfb449caa4a0be7464ae70d0a
Joshua Gort <accounts@joshuagort.com>
2026-09-16 10:34:54 +0200

    Cherry picked upstream changes

--- docker/domserver/build.sh
+++ docker/domserver/build.sh
@@ -6,17 +6,14 @@
 cd /domjudge-src/domjudge*
 chown -R domjudge: .
 # If we used a local source tarball, it might not have been built yet
-sudo -u domjudge sh -c '. /venv/bin/activate && make dist'
-sudo -u domjudge ./configure -with-baseurl=http://dj.chipcie.ch.tudelft.nl/
+sudo -u domjudge make dist
+sudo -u domjudge ./configure --with-baseurl=http://dj.chipcie.ch.tudelft.nl/ --disable-judgehost-build --with-install-method=docker
 
 # Passwords should not be included in the built image. We create empty files here to prevent passwords from being generated.
 sudo -u domjudge touch etc/dbpasswords.secret etc/restapi.secret etc/symfony_app.secret etc/initial_admin_password.secret
 
-sudo -u domjudge make domserver
-make install-domserver
+sudo -u domjudge make domserver docs
+make install-domserver install-docs
 
 # Remove installed password files
 rm /opt/domjudge/domserver/etc/*.secret
-
-sudo -u domjudge sh -c '. /venv/bin/activate && make docs'
-make install-docs
